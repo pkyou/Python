8beaf1aaec9fadd908a5727734403a426b5047e6
pkyou <471162774@qq.com>
2016-12-26 21:22:00 +0800

    add business
    Signed-off-by: pkyou <471162774@qq.com>

--- Site/mysite/MyInformationApp/views.py
+++ Site/mysite/MyInformationApp/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+import OutputString
 
 def HelloSite(resquest):
-    return HttpResponse("<h1> Hello, my site by pk</h1>")
+    people = OutputString.People()
+
+    age = '%d'%people.age
+    return HttpResponse("<h1> Hello, my site by pk </h1>"+age)
