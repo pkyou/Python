d02d5f76117d2a4fb397c71b26e5aefdb241fdde
pkyou <471162774@qq.com>
2017-01-15 21:43:29 +0800

    get x-y=2 between min value and max value
    Signed-off-by: pkyou <471162774@qq.com>

--- MathCalc/GetTheBestAnswer.py
+++ MathCalc/GetTheBestAnswer.py
@@ -1,11 +1,28 @@
 
 
 class GetAnswer:
+    minValue = 0
+    maxValue = 0
     currentName = 212
+    resultString = ""
     def GetTestResult(self):
 
         return GetAnswer.currentName
-    def __init__(self):
+    def __init__(self,minValue,maxValue):
+        self.minValue = minValue
+        self.maxValue = maxValue
         pass
+    def GetAll(self):
 
-    
+        for currentValue in range(self.minValue,self.maxValue):
+            self.resultString += self.GetSingle(currentValue)
+        return self.resultString
+
+
+    def GetSingle(self,firstValue):
+        if not ( self.minValue < firstValue < self.maxValue):
+            return ""
+        for i in range(self.minValue,self.maxValue):
+            if firstValue - i == 2:
+                return "%d,%d"%(firstValue,i) +"\r\n"
+        return ""
