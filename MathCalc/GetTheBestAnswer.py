a069c107f625620fbeb91e119846e2ce0a533f47
pkyou <471162774@qq.com>
2017-01-15 22:49:43 +0800

    1-8 addition
    Signed-off-by: pkyou <471162774@qq.com>

--- MathCalc/GetTheBestAnswer.py
+++ MathCalc/GetTheBestAnswer.py
@@ -1,28 +1,36 @@
-
-
 class GetAnswer:
     minValue = 0
     maxValue = 0
     currentName = 212
     resultString = ""
-    def GetTestResult(self):
 
+    def GetTestResult(self):
         return GetAnswer.currentName
-    def __init__(self,minValue,maxValue):
+
+    def __init__(self, minValue, maxValue):
         self.minValue = minValue
         self.maxValue = maxValue
         pass
+
     def GetAll(self):
 
-        for currentValue in range(self.minValue,self.maxValue):
+        for currentValue in range(self.minValue, self.maxValue):
             self.resultString += self.GetSingle(currentValue)
         return self.resultString
-
-
-    def GetSingle(self,firstValue):
-        if not ( self.minValue < firstValue < self.maxValue):
+    # 满足条件的数字集合
+    def GetSingle(self, firstValue):
+        if not (self.minValue < firstValue < self.maxValue):
             return ""
-        for i in range(self.minValue,self.maxValue):
+        for i in range(self.minValue, self.maxValue):
             if firstValue - i == 2:
-                return "%d,%d"%(firstValue,i) +"\r\n"
+                return "%d,%d" % (firstValue, i) + "\r\n"
         return ""
+
+    def GetInitAnswer(self):
+        pass
+
+    def AdditionCheck(self,checkedValue1,checkedValue2,targetValue):
+        return checkedValue1 + checkedValue2 == targetValue
+    def SubtractionCheck(self,checkedValue1,checkedValue2,targetValue):
+        return checkedValue1 - checkedValue2 == targetValue
+
